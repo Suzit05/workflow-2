dca071957699e6494f5b43c8e0b64bc53cb5241e
Suzit05 <103447157+Suzit05@users.noreply.github.com>
2025-07-28 13:58:50 +0530

    node app, pipeline added

--- file_writer.py
+++
@@ -1,6 +0,0 @@
-def write_to_file(filename,content):
-    with open(filename, "w") as f:
-        f.write(content)
-if __name__ == "__main__":
-    write_to_file("output.txt","Hello from Github actions")
-    print("File created and Content written.")
---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+const assert = require("assert")
+const isEven = require("../src/index")
+
+//using mocha -testing
+describe("is Even function",()=>{
+    it('should return true for isEven(2)', ()=>{
+        assert.strictEqual(isEven(2),true)
+    })
+    it('should return true for isEven(5)', ()=>{
+        assert.strictEqual(isEven(5),false)
+    })
+    it('should return true for isEven(42)', ()=>{
+        assert.strictEqual(isEven(42),true)
+    })
+})
